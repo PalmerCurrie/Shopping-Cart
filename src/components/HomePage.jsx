8b2b3153f18e4797206807f3f4c1bdf4f479e6a9
PalmerCurrie <palmercurrie@gmail.com>
2024-07-07 10:28:15 -0700

    Updated text in homepage and cart page

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -4,7 +4,7 @@ import '../styles/HomePage.css';
 function HomePage() {
   return (
     <div className='home-container'>
-      <h1>Welcome to Cart</h1>
+      <h1>Welcome to cart.</h1>
       <p className='home-description'>An online shopping experience for all your needs!</p>
     </div>
   );
